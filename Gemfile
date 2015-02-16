f25d43ba80657633f01627e116348c94599fe922
Chad Miles <milesch4@msu.edu>
2015-02-16 13:58:35 -0500

    inital commit

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 ruby '2.2.0'
 
 gem 'sinatra', '~> 1.4.5'
+gem 'holidapi'
+gem 'rack', '1.5.2'
--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require 'sinatra'
+require 'holidapi'
 
 class MyWebApp < Sinatra::Base
 	get '/' do
+    time = Time.new
+    @holidayscurrent = HolidApi.get(year: time.year, month: time.month)
+    @holidaysdob = HolidApi.get(year: 1994, month: 5)
 		erb :"index#{ rand(3) + 1 }"
 	end
 end
