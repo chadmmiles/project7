014d57a192dbb78d851a54b0a32e574b67e7dede
Chad Miles <milesch4@msu.edu>
2015-02-23 13:32:08 -0500

    internal server error handling

--- app.rb
+++ app.rb
@@ -3,10 +3,12 @@ require 'holidapi'
 
 class MyWebApp < Sinatra::Base
 	get '/' do
-    puts params
     time = Time.new
-    @holidayscurrent = HolidApi.get(year: time.year, month: time.month)
-    @holidaysdob = HolidApi.get(country: params['country'], year: params['year'], month: params['month'])
+    params[:country] ||="US"
+    params[:year] ||=time.year
+    params[:month] ||=time.month
+    puts params
+    @holidays = HolidApi.get(country: params[:country], year: params[:year], month: params[:month])
 		erb :"index#{ rand(3) + 1 }"
 	end
 end
