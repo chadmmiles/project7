352d4fac285e2eb756f6f1db9ed975f0ed1c8f91
Chad Miles <milesch4@msu.edu>
2015-02-23 12:24:39 -0500

    initial commit

--- app.rb
+++ app.rb
@@ -3,9 +3,10 @@ require 'holidapi'
 
 class MyWebApp < Sinatra::Base
 	get '/' do
+    puts params
     time = Time.new
     @holidayscurrent = HolidApi.get(year: time.year, month: time.month)
-    @holidaysdob = HolidApi.get(year: 1994, month: 5)
+    @holidaysdob = HolidApi.get(country: params['country'], year: params['year'], month: params['month'])
 		erb :"index#{ rand(3) + 1 }"
 	end
 end
